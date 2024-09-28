9680c9b960a05f13fee714702dd696c5fbdc3cb8
Vanya Chernov <vanyachernov2001@gmail.com>
2024-09-28 22:17:31 +0300

    Add abstract Tool. Implement Brush

---
+++ frontend/src/tools/Brush.ts
@@ -0,0 +1,39 @@
+import Tool from "./Tool.ts";
+
+export default class Brush extends Tool {
+    private mouseDown: boolean;
+    
+    constructor(canvas: any) {
+        super(canvas);
+        this.listen();
+        this.mouseDown = false;
+    }
+    
+    listen() {
+        this.canvas.onmousemove = this.mouseMoveHandler.bind(this);
+        this.canvas.onmouseup = this.mouseUpHandler.bind(this);
+        this.canvas.onmousedown = this.mouseDownHandler.bind(this);
+    }
+    
+    private mouseUpHandler(e: any) {
+        this.mouseDown = false;
+    }
+
+    private mouseDownHandler(e: any) {
+        this.mouseDown = true;
+        this.ctx.beginPath();
+        this.ctx.moveTo(e.pageX - e.target.offsetLeft, e.pageY - e.target.offsetTop);
+    }
+
+    private mouseMoveHandler(e: any) {
+        if (this.mouseDown) {
+            this.draw(e.pageX - e.target.offsetLeft, e.pageY - e.target.offsetTop);
+        }
+    }
+
+    private draw(x: number, y: number) {
+        this.ctx.lineTo(x, y);
+        this.ctx.stroke();
+        console.log("drawing...");
+    }
+}
