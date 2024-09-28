540e8fe6dafc8027d2d01cab37ab7f03a457c02d
Vanya Chernov <vanyachernov2001@gmail.com>
2024-09-28 23:07:10 +0300

    Implement Rect figure

--- frontend/src/components/Toolbar.tsx
+++ frontend/src/components/Toolbar.tsx
@@ -1,16 +1,23 @@
-import { Box, IconButton, HStack } from '@chakra-ui/react';
-import { FaPencilAlt } from 'react-icons/fa';
-import toolState from "../store/toolState.ts";
-import Brush from "../tools/Brush.ts";
-import canvasState from "../store/canvasState.ts";
 import {useState} from "react";
+import {Box, IconButton, HStack, Flex} from '@chakra-ui/react';
+import { FaPencilAlt } from 'react-icons/fa';
+import { RiRectangleFill } from "react-icons/ri";
+import toolState from "../store/toolState.ts";
+import canvasState from "../store/canvasState.ts";
+import Brush from "../tools/Brush.ts";
+import Rect from "../tools/Rect.ts";
 
 const Toolbar = () => {
     const [selectedTool, setSelectedTool] = useState<string>("");
     
-    const handleSelectTool = (tool: string) => {
-        setSelectedTool(tool);
+    const handleSelectBrush = () => {
+        setSelectedTool("pencil");
         toolState.setTool(new Brush(canvasState.canvas));
+    }
+
+    const handleSelectRect = () => {
+        setSelectedTool("rectangle");
+        toolState.setTool(new Rect(canvasState.canvas));
     }
     
     return (
@@ -19,17 +26,34 @@ const Toolbar = () => {
             bg="white.50"
             p={4}
         >
-            <HStack spacing={4} justify="left">
-                <IconButton
-                    icon={<FaPencilAlt />}
-                    bg='green.600'
-                    color='white'
-                    aria-label="Pencil"
-                    _hover={selectedTool !== 'pencil' ? { bg: 'green.500' } : { bg: 'gray.300' }}
-                    onClick={() => handleSelectTool("pencil")}
-                    isActive={selectedTool === 'pencil'}
-                />
-            </HStack>
+            <Box className="w-100">
+                <Flex gap={3}>
+                    <HStack spacing={4} justify="left">
+                        <IconButton
+                            icon={<FaPencilAlt />}
+                            bg='green.600'
+                            color='white'
+                            aria-label="Pencil"
+                            _hover={selectedTool !== 'pencil' ? { bg: 'green.500' } : { bg: 'gray.300' }}
+                            onClick={handleSelectBrush}
+                            isActive={selectedTool === 'pencil'}
+                        />
+                    </HStack>
+
+                    <HStack spacing={4} justify="left">
+                        <IconButton
+                            icon={<RiRectangleFill />}
+                            bg='green.600'
+                            color='white'
+                            aria-label="Rectangle"
+                            _hover={selectedTool !== 'rectangle' ? { bg: 'green.500' } : { bg: 'gray.300' }}
+                            onClick={handleSelectRect}
+                            isActive={selectedTool === 'rectangle'}
+                        />
+                    </HStack>
+                </Flex>
+            </Box>
+            
         </Box>
     );
 };
